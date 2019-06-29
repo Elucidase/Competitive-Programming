9ca38ebcc1e0afdf393f86fe390ffb40b00742f7
Elucidase <51693403+Elucidase@users.noreply.github.com>
2019-06-30 01:02:26 +0800

    Update and rename Median of Two Sorted Arrays to Median of Two Sorted Arrays.js

--- Median of Two Sorted Arrays.js
+++ Median of Two Sorted Arrays.js
@@ -3,7 +3,7 @@ LeetCode problem: Median of Two Sorted Arrays
 There are two sorted arrays nums1 and nums2 of size m and n respectively.
 Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
 You may assume nums1 and nums2 cannot be both empty.
-*
+*/
 let findMedianSortedArrays = function(nums1, nums2) {
     let index1, index2, result1, result2;
     let length = nums1.length + nums2.length;
